c97a801c9dcfbf99d2809a85c783d1c09ff90b5c
Jess Poemape <jesspoemape@gmail.com>
2017-06-13 09:09:19 -0600

    Completed project

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -8,11 +8,14 @@ export default class ColorChanger extends Component {
     };
   }
 
-  // componentWillReceiveProps
+// children don't update their value based on the edit toggle without this method
+  componentWillReceiveProps(props) {
+    this.setState({allowEdit: props.allowEdit});
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => this.props.update(e.target.value) } disabled={ this.state.allowEdit === "false" }>
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -8,11 +8,14 @@ export default class SizeChanger extends Component {
     };
   }
 
-  // componentWillReceiveProps
+// children don't update their value based on the edit toggle without this method
+  componentWillReceiveProps(props) {
+    this.setState({allowEdit: props.allowEdit});
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => this.props.update((e.target.value)/1) } disabled={ this.state.allowEdit === "false" }>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
