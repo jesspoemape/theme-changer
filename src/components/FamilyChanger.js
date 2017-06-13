88ee5a80b749562e94fd89b25a662b41860f370b
Jess Poemape <jesspoemape@gmail.com>
2017-06-13 09:22:43 -0600

    Added verdana instead of courier font

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -18,7 +18,7 @@ export default class FamilyChanger extends Component {
       <select className="dropDownContainer" onChange={ (e) => this.props.update(e.target.value) } disabled={ this.state.allowEdit === "false" }>
         <option value="monospace"> Monospace </option>
         <option value="arial"> Arial </option>
-        <option value="courier"> Courier </option>
+        <option value="verdana"> Verdana </option>
       </select>
     )
   }
